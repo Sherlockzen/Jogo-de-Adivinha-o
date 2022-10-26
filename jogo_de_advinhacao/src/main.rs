94738346dd56e898ce3090cd3e299f785d9fc9fd
Hewerton <hewertonlourenco@gmail.com>
2022-10-26 00:11:56 -0300

    Finishing the project

--- jogo_de_advinhacao/src/main.rs
+++ jogo_de_advinhacao/src/main.rs
@@ -1,14 +1,39 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Advinhe o numero!");
 
-    println!("Digite o seu palpite.");
+    let numero_secreto = rand::thread_rng().gen_range(1, 101);
 
-    let mut palpite = String::new();
+    println!("O número secreto é: {}", numero_secreto);
 
-    io::stdin().read_line(&mut palpite)
-        .expect("Falha ao ler entrada");
 
-    println!("Voce disse: {}", palpite);
+    loop {
+        println!("Digite o seu palpite.");
+
+        let mut palpite = String::new();
+
+        io::stdin().read_line(&mut palpite)
+            .expect("Falha ao ler entrada");
+
+        let palpite: u32 = match palpite.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };    
+
+        println!("Voce disse: {}", palpite);
+
+        match palpite.cmp(&numero_secreto) {
+            Ordering::Less => println!("Muito baixo!"),
+            Ordering::Greater => println!("Muito alto!"),
+            Ordering::Equal => {
+                println!("Você acertou!");
+                break;
+            }
+        }
+    }
 }
